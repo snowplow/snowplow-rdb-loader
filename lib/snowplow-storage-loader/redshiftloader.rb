39fc5a5dcbb1983957e87686df0838cb30e2e7b4
Alex Dean <alexander.dean@keplarllp.com>
2013-02-25 19:57:22 +0000

    Simplified RedshiftLoader code

--- lib/snowplow-storage-loader/redshiftloader.rb
+++ lib/snowplow-storage-loader/redshiftloader.rb
@@ -18,35 +18,29 @@ module SnowPlow
   module StorageLoader
     module RedshiftLoader
 
+      # Constants for the load process
+      EVENT_FIELD_SEPARATOR = "\\t"
+      JISQL_PATH = File.join("..", "jisql-2.0.11")
+
       def load_events(config)
         puts "Loading SnowPlow events into Redshift..."
 
         # Assemble the relevant parameters for the bulk load query
-
-        jdbc_url = "jdbc:postgresql://" + config[:targets][:Redshift][:endpoint] + ":" + config[:targets][:Redshift][:port].to_s + "/" + config[:targets][:Redshift][:database]
-
-        source_in_s3 = config[:s3][:buckets][:in]
-
-        output_table_in_redshift = config[:targets][:Redshift][:table]
-
-        credentials = "'aws_access_key_id=" + config[:aws][:access_key_id] + ";aws_secret_access_key=" + config[:aws][:secret_access_key] + "'"
-
-        delimiter = "'\\t'"
-
-        query = "copy " + output_table_in_redshift + " from '" + source_in_s3 + "' credentials " + credentials + " delimiter " + delimiter + ";"
-
-        username = config[:targets][:Redshift][:username]
-        password = config[:targets][:Redshift][:password]
-
-        # We need to execute the following request at the command line:
-
-        cmd_line_request = %Q!java -cp 4-storage/storage-loader/jisql-2.0.11/lib/jisql-2.0.11.jar:4-storage/storage-loader/jisql-2.0.11/lib/jopt-simple-3.2.jar:4-storage/storage-loader/jisql-2.0.11/lib/postgresql-8.4-703.jdbc4.jar com.xigole.util.sql.Jisql -driver postgresql -cstring #{jdbc_url} -user #{username} -password #{password} -c \\; -query "#{query}"!
-
-        stdout_err = `#{cmd_line_request} 2>&1` # Execute the cmd_line_request
+        jdbc_url = "jdbc:postgresql://%s:%s/%s" % [config[:storage][:endpoint], config[:storage][:port], config[:storage][:database]]
+        credentials = "aws_access_key_id=%s;aws_secret_access_key=%s" % [config[:aws][:access_key_id], config[:aws][:secret_access_key]]
+        query = "copy %s from '%s' credentials '%s' delimiter '%s';" % [config[:storage][:table], config[:s3][:buckets][:in], credentials, EVENT_FIELD_SEPARATOR]
+        username = config[:storage][:username]
+        password = config[:storage][:password]
+           
+        # Execute the following request at the command line:
+        jisql_cmd = %Q!java -cp #{JISQL_PATH}/* com.xigole.util.sql.Jisql -driver postgresql -cstring #{jdbc_url} -user #{username} -password #{password} -c \\; -query "#{query}"!
+        stdout_err = `#{jisql_cmd} 2>&1` # Execute
         ret_val = $?.to_i
-          unless ret_val == 0
-            raise StorageLoader::Loader::DatabaseLoadError, "Error code #{ret_val}: #{stdout_err}"
-          end
+
+        # Error handling
+        unless ret_val == 0
+          raise DatabaseLoadError, "Error code #{ret_val}: #{stdout_err}"
+        end
       end
     end
   end
