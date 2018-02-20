8519e73eeabc0cddb64b2b778df79a026a583549
Anton Parkhomenko <mailbox@chuwy.me>
2018-02-21 00:54:00 +0700

    RDB Loader: escape input for sanitize function (close #87)

--- src/test/scala/com/snowplowanalytics/snowplow/rdbloader/utils/CommonSpec.scala
+++ src/test/scala/com/snowplowanalytics/snowplow/rdbloader/utils/CommonSpec.scala
@@ -17,11 +17,18 @@ import org.specs2.Specification
 
 class CommonSpec extends Specification { def is = s2"""
   Sanitize message $e1
+  Sanitize message that contains invalid regular expression $e2
   """
 
   def e1 = {
     val message = "Outputpassword. Output username"
     val result = Common.sanitize(message, List("password", "username"))
     result must beEqualTo("Outputxxxxxxxx. Output xxxxxxxx")
+  }
+
+  def e2 = {
+    val message = "Output$**^. Output username"
+    val result = Common.sanitize(message, List("""$**^""", "username"))
+    result must beEqualTo("Outputxxxx. Output xxxxxxxx")
   }
 }
