9205e10bb84d38b4c731dc24f41b7d13dc71c793
Sajith Appukuttan <asajith@gmail.com>
2018-10-20 18:50:39 -0700

    RDB Shredder: bump Spark to 2.3.2 (close #162)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,8 +31,8 @@ object Dependencies {
 
     // Scala (Shredder)
     val analyticsSdk     = "0.4.2-M1"
-    val spark            = "2.2.0"
-    val eventsManifest   = "0.2.0-M2"
+    val spark            = "2.3.2"
+    val eventsManifest   = "0.2.0"
     val schemaDdl        = "0.9.0"
 
     // Java (Loader)
