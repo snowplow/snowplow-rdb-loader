08f025cb6e91201b95772069fa61a9e889b7fe70
Anton Parkhomenko <mailbox@chuwy.me>
2019-04-05 18:51:39 +0700

    RDB Shredder: factor out scala-common-enrich (close #138)

--- shredder/src/test/scala/com.snowplowanalytics.snowplow.storage.spark/bad/InvalidEnrichedEventsSpec.scala
+++ shredder/src/test/scala/com.snowplowanalytics.snowplow.storage.spark/bad/InvalidEnrichedEventsSpec.scala
@@ -17,16 +17,22 @@ package bad
 
 import java.io.File
 
+import io.circe.literal._
+
 import org.specs2.mutable.Specification
 
 object InvalidEnrichedEventsSpec {
   import ShredJobSpec._
   val lines = Lines(
-    """snowplowweb	web	2014-06-01 18:04:11.639	29th May 2013 18:04:12	2014-05-29 18:04:11.639	page_view	not-a-uuid	836413	clojure	js-2.0.0-M2	clj-0.6.0-tom-0.0.4	hadoop-0.5.0-common-0.4.0		216.207.42.134	3499345421	3b1d1a375044eede	3	2bad2a4e-aae4-4bea-8acd-399e7fe0366a	US	CA	South San Francisco		37.654694	-122.4077						http://snowplowanalytics.com/blog/2013/02/08/writing-hive-udfs-and-serdes/	Writing Hive UDFs - a tutorial		http	snowplowanalytics.com	80	/blog/2013/02/08/writing-hive-udfs-and-serdes/																	{"schema":"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-0","data":[{"schema":"iglu:com.snowplowanalytics.website/fake_context/jsonschema/1-0-0","data":{"author":"Alex Dean","topics":["hive","udf","serde","java","hadoop"],"subCategory":"inside the plow","category":"blog","whenPublished":"2013-02-08"}}]}																									Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14	Safari	Safari		Browser	WEBKIT	en-us	0	0	0	0	0	0	0	0	0	1	24	1440	1845	Mac OS	Mac OS	Apple Inc.	America/Los_Angeles	Computer	0					"""
+    """snowplowweb	web	2014-06-01 18:04:11.639	29th May 2013 18:04:12	2014-05-29 18:04:11.639	page_view	not-a-uuid	836413	clojure	js-2.0.0-M2	clj-0.6.0-tom-0.0.4	hadoop-0.5.0-common-0.4.0		216.207.42.134	3499345421	3b1d1a375044eede	3	2bad2a4e-aae4-4bea-8acd-399e7fe0366a	US	CA	South San Francisco		37.654694	-122.4077						http://snowplowanalytics.com/blog/2013/02/08/writing-hive-udfs-and-serdes/	Writing Hive UDFs - a tutorial		http	snowplowanalytics.com	80	/blog/2013/02/08/writing-hive-udfs-and-serdes/																	{"schema":"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-0","data":[{"schema":"iglu:com.snowplowanalytics.website/fake_context/jsonschema/1-0-0","data":{"author":"Alex Dean","topics":["hive","udf","serde","java","hadoop"],"subCategory":"inside the plow","category":"blog","whenPublished":"2013-02-08"}}]}																									Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14	Safari	Safari		Browser	WEBKIT	en-us	0	0	0	0	0	0	0	0	0	1	24	1440	1845	Mac OS	Mac OS	Apple Inc.	America/Los_Angeles	Computer	0																												"""
   )
-  val expected = (line: String) =>
-    """{"line":"%s","errors":[{"level":"error","message":"Field [event_id]: [not-a-uuid] is not a valid UUID"},{"level":"error","message":"Field [collector_tstamp]: [29th May 2013 18:04:12] is not in the expected Redshift/Postgres timestamp format"}]}"""
-      .format(line.replaceAll("\"", "\\\\\"")).replaceAll("[\n\r]","").replaceAll("[\t]","\\\\t")
+  val expected = json"""{
+     "original":"snowplowweb\tweb\t2014-06-01 18:04:11.639\t29th May 2013 18:04:12\t2014-05-29 18:04:11.639\tpage_view\tnot-a-uuid\t836413\tclojure\tjs-2.0.0-M2\tclj-0.6.0-tom-0.0.4\thadoop-0.5.0-common-0.4.0\t\t216.207.42.134\t3499345421\t3b1d1a375044eede\t3\t2bad2a4e-aae4-4bea-8acd-399e7fe0366a\tUS\tCA\tSouth San Francisco\t\t37.654694\t-122.4077\t\t\t\t\t\thttp://snowplowanalytics.com/blog/2013/02/08/writing-hive-udfs-and-serdes/\tWriting Hive UDFs - a tutorial\t\thttp\tsnowplowanalytics.com\t80\t/blog/2013/02/08/writing-hive-udfs-and-serdes/\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{\"schema\":\"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-0\",\"data\":[{\"schema\":\"iglu:com.snowplowanalytics.website/fake_context/jsonschema/1-0-0\",\"data\":{\"author\":\"Alex Dean\",\"topics\":[\"hive\",\"udf\",\"serde\",\"java\",\"hadoop\"],\"subCategory\":\"inside the plow\",\"category\":\"blog\",\"whenPublished\":\"2013-02-08\"}}]}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tMozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14\tSafari\tSafari\t\tBrowser\tWEBKIT\ten-us\t0\t0\t0\t0\t0\t0\t0\t0\t0\t1\t24\t1440\t1845\tMac OS\tMac OS\tApple Inc.\tAmerica/Los_Angeles\tComputer\t0\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
+     "errors":[
+        "Cannot parse key 'collector_tstamp with value 29th May 2013 18:04:12 into datetime",
+        "Cannot parse key 'event_id with value not-a-uuid into UUID"
+     ]
+  }""".noSpaces
 }
 
 class InvalidEnrichedEventsSpec extends Specification with ShredJobSpec {
@@ -38,8 +44,7 @@ class InvalidEnrichedEventsSpec extends Specification with ShredJobSpec {
 
     "write a bad row JSON with input line and error message for each input line" in {
       val Some((jsons, _)) = readPartFile(dirs.badRows, "")
-      jsons.map(removeTstamp) must_==
-        InvalidEnrichedEventsSpec.lines.lines.map(InvalidEnrichedEventsSpec.expected)
+      jsons must beEqualTo(List(InvalidEnrichedEventsSpec.expected))
     }
 
     "not write any atomic-events" in {
