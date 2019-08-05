6ba7307c4d580e452dc6974c4d284ed6c38b9051
Anton Parkhomenko <mailbox@chuwy.me>
2019-08-05 20:03:34 +0700

    jdbc again

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
 
     // Java (Loader)
     val postgres         = "42.0.0"
-    val redshift         = "1.2.20.1043"
+    val redshift         = "1.2.32.1056"
     val aws              = "1.11.319"
     val jSch             = "0.1.54"
 
