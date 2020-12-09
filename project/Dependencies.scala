6b782c6faef9542720621e8f28890e72e7fc63c0
Scala Steward <me@scala-steward.org>
2020-12-09 15:01:28 +0100

    Bump scalacheck from 1.14.0 to 1.14.3 (close #243)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
 
     // Scala (test only)
     val specs2           = "4.10.5"
-    val scalaCheck       = "1.14.0"
+    val scalaCheck       = "1.14.3"
   }
 
   val resolutionRepos = Seq(
