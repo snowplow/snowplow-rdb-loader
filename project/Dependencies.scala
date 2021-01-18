215ebaa611d4e05c16a965c03685d6580382703a
Scala Steward <me@scala-steward.org>
2021-01-18 21:43:31 +0100

    Bump specs2-core, specs2-scalacheck from 4.10.5 to 4.10.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     val jSch             = "0.1.55"
 
     // Scala (test only)
-    val specs2           = "4.10.5"
+    val specs2           = "4.10.6"
     val scalaCheck       = "1.14.0"
   }
 
