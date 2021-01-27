9dd5f5d845c8dc889226ffd3288b8c6525cd60ef
Scala Steward <me@scala-steward.org>
2021-01-27 04:49:33 +0100

    Bump kind-projector from 0.9.10 to 0.10.0

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -48,7 +48,7 @@ object BuildSettings {
       "-encoding", "UTF-8"
     ),
 
-    addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.10" cross CrossVersion.binary)
+    addCompilerPlugin("org.typelevel" % "kind-projector" % "0.10.0" cross CrossVersion.binary)
   )
 
   // sbt-assembly settings
