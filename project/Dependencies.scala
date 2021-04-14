ac7f6877bf3125b53542608e1c9398e3037daca6
Scala Steward <me@scala-steward.org>
2021-04-14 22:40:15 +0200

    Bump sentry from 3.2.0 to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
     val aws              = "1.11.990"
     val aws2             = "2.16.23"
     val jSch             = "0.1.55"
-    val sentry           = "3.2.0"
+    val sentry           = "3.2.1"
 
     // Scala (test only)
     val specs2           = "4.10.5"
