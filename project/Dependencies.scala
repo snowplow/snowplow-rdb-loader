a5759045876233f923ecb3b2f3d0f79ff9a04229
Scala Steward <me@scala-steward.org>
2021-04-14 22:39:39 +0200

    Bump monocle-core, monocle-macro from 2.0.3 to 2.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
     val fs2Aws           = "3.0.11"
     val fs2Blobstore     = "0.7.3"
     val doobie           = "0.12.1"
-    val monocle          = "2.0.3"
+    val monocle          = "2.0.5"
     val catsRetry        = "2.1.0"
     val log4cats         = "1.2.0"
     val http4s           = "0.21.21"
