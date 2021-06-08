c08398d504fb7f5469a7e363523c928e2eb9a975
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2021-06-08 19:51:42 +0300

    RDB Shredder: integrate Sentry (close #510)

--- modules/shredder/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/shredder/batch/Main.scala
+++ modules/shredder/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/shredder/batch/Main.scala
@@ -14,9 +14,13 @@
  */
 package com.snowplowanalytics.snowplow.rdbloader.shredder.batch
 
+import io.sentry.{Sentry, SentryOptions}
+
 import org.apache.spark.SparkConf
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.SparkSession
+
+import cats.syntax.either._
 
 import com.snowplowanalytics.snowplow.rdbloader.common.config.{Config, ShredderCliConfig}
 import com.snowplowanalytics.snowplow.rdbloader.shredder.batch.spark.Serialization
@@ -38,8 +42,16 @@ object Main {
             val spark = SparkSession.builder()
               .config(sparkConfig)
               .getOrCreate()
-            ShredJob.run(spark, cli.igluConfig, cli.duplicateStorageConfig, cli.config.formats, b, cli.config.messageQueue, cli.config.region)
-            spark.stop()
+            val sentryClient = cli.config.monitoring.sentry.map(s => Sentry.init(SentryOptions.defaults(s.dsn.toString)))
+            Either
+              .catchNonFatal(ShredJob.run(spark, cli.igluConfig, cli.duplicateStorageConfig, cli.config, b)) match {
+                case Left(throwable) =>
+                  sentryClient.foreach(_.sendException(throwable))
+                  spark.stop()
+                  throw throwable
+                case Right(_) =>
+                  spark.stop()
+              }
           case other =>
             System.err.println(s"Shredder configuration $other is not for Spark")
             System.exit(2)
