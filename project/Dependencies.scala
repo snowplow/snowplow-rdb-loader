b76fa7faea0e6543f881d13d5150911a6f3656e7
Scala Steward <me@scala-steward.org>
2021-07-12 20:44:37 +0200

    RDB Loader: bump redshift-jdbc42-no-awssdk from 1.2.54.1082 to 1.2.55.1083 (close #535)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
 
     // Java (Loader)
     val slf4j            = "1.7.32"
-    val redshift         = "1.2.54.1082"
+    val redshift         = "1.2.55.1083"
     val aws              = "1.11.1019"
     val aws2             = "2.16.23"
     val jSch             = "0.1.55"
