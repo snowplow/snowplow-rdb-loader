00b2ff5c81d8690432fb195e77eba6c07440f571
Anton Parkhomenko <mailbox@chuwy.me>
2021-07-21 15:26:18 +0300

    RDB Shredder: skip CrossBatchDeduplicationSpec (close #462)

--- modules/shredder/src/test/scala/com/snowplowanalytics/snowplow/rdbloader/shredder/batch/good/CrossBatchDeduplicationSpec.scala
+++ modules/shredder/src/test/scala/com/snowplowanalytics/snowplow/rdbloader/shredder/batch/good/CrossBatchDeduplicationSpec.scala
@@ -224,6 +224,8 @@ object CrossBatchDeduplicationSpec {
  * @see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html
  */
 class CrossBatchDeduplicationSpec extends Specification with ShredJobSpec {
+  args(skipAll = true)
+  
   override def appName = "cross-batch-deduplication"
   CrossBatchDeduplicationSpec.Storage.prepareLocalTable()
   sequential
