d84286943dd47c6a54288fd4a33335d828792639
Scala Steward <me@scala-steward.org>
2021-09-03 11:09:01 +0200

    Bump log4cats-slf4j from 1.3.0 to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     val doobie           = "0.13.4"
     val monocle          = "2.0.3"
     val catsRetry        = "2.1.0"
-    val log4cats         = "1.3.0"
+    val log4cats         = "1.3.1"
     val http4s           = "0.21.25"
     val scalaTracker     = "1.0.0"
 
