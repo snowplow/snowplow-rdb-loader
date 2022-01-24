bf4b811e51f286d45b55e45390a9947427a8ccad
spenes <enes@snowplowanalytics.com>
2022-01-25 00:57:05 +0300

    Implement MigrationBuilder for Snowflake

--- modules/snowflake-loader/src/main/scala/com/snowplowanalytics/snowplow/loader/snowflake/db/ast/Ddl.scala
+++ modules/snowflake-loader/src/main/scala/com/snowplowanalytics/snowplow/loader/snowflake/db/ast/Ddl.scala
@@ -1,5 +1,17 @@
 package com.snowplowanalytics.snowplow.loader.snowflake.db.ast
 
+import doobie.Fragment
+import com.snowplowanalytics.snowplow.loader.snowflake.db.Statement
+
 trait Ddl {
   def toDdl: String
+}
+
+object Ddl {
+  implicit class DdlStatement(ddl: Ddl) {
+    def toStatement: Statement = new Statement {
+      override def toFragment: Fragment =
+        Fragment.const0(ddl.toDdl)
+    }
+  }
 }
---
+++ modules/snowflake-loader/src/test/scala/com/snowplowanalytics/snowplow/loader/snowflake/test/PureDAO.scala
@@ -0,0 +1,47 @@
+package com.snowplowanalytics.snowplow.loader.snowflake.test
+
+import com.snowplowanalytics.snowplow.rdbloader.test.{Pure, PureTransaction, TestState}
+import com.snowplowanalytics.snowplow.loader.snowflake.db.{SfDao, Statement}
+import doobie.Read
+
+case class PureDAO(executeQuery: Statement => Pure[Any], executeUpdate: Statement => Pure[Int])
+
+object PureDAO {
+
+  def getResult(s: TestState)(query: Statement): Any =
+    query match {
+      case Statement.TableExists(_, _)       => false
+      case Statement.GetColumns(_, _)        => List("some_column")
+      case Statement.ManifestGet(_, _)       => None
+      case _                                 => throw new IllegalArgumentException(s"Unexpected query $query with ${s.getLog}")
+    }
+
+  def custom(getResult: TestState => Statement => Any): PureDAO = {
+    def executeQuery(query: Statement): Pure[Any] =
+      Pure((s: TestState) => (s.log(query.toFragment.toString()), getResult(s)(query).asInstanceOf[Any]))
+
+    def executeUpdate(sql: Statement): Pure[Int] =
+      Pure((s: TestState) => (s.log(sql.toFragment.toString()), 1))
+
+    PureDAO(q => executeQuery(q), executeUpdate)
+  }
+
+  val init: PureDAO = custom(getResult)
+
+  def interpreter(results: PureDAO): SfDao[Pure] = new SfDao[Pure] {
+    def executeUpdate(sql: Statement): Pure[Int] =
+      results.executeUpdate(sql)
+
+    def executeQuery[A](query: Statement)(implicit A: Read[A]): Pure[A] =
+      results.executeQuery.asInstanceOf[Statement => Pure[A]](query)
+
+    def executeQueryList[A](query: Statement)(implicit A: Read[A]): Pure[List[A]] =
+      results.executeQuery.asInstanceOf[Statement => Pure[List[A]]](query)
+
+    def executeQueryOption[A](query: Statement)(implicit A: Read[A]): Pure[Option[A]] =
+      results.executeQuery.asInstanceOf[Statement => Pure[Option[A]]](query)
+
+    def rollback: Pure[Unit] =
+      Pure.modify(_.log(PureTransaction.Rollback))
+  }
+}
