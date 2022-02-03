af60e5a6fb30ae78ff28bf9a4f529af3a0264361
spenes <enes@snowplowanalytics.com>
2022-02-03 16:09:18 +0300

    Snowflake Loader

---
+++ modules/snowflake-loader/src/test/scala/com/snowplowanalytics/snowplow/loader/snowflake/db/SnowflakeMigrationSpec.scala
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2022-2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.loader.snowflake.db
+
+import cats.syntax.all._
+import cats.data.NonEmptyList
+
+import com.snowplowanalytics.iglu.core.SchemaVer.Full
+import com.snowplowanalytics.iglu.core.{SchemaKey, SchemaMap, SelfDescribingSchema}
+import com.snowplowanalytics.iglu.schemaddl.IgluSchema
+import com.snowplowanalytics.iglu.schemaddl.jsonschema.Schema
+import com.snowplowanalytics.iglu.schemaddl.jsonschema.properties._
+import com.snowplowanalytics.iglu.schemaddl.migrations.{SchemaList => DSchemaList}
+import com.snowplowanalytics.snowplow.rdbloader.LoaderError
+import com.snowplowanalytics.snowplow.rdbloader.test.{Pure, TestState}
+import com.snowplowanalytics.snowplow.rdbloader.test.TestState.LogEntry
+import com.snowplowanalytics.snowplow.rdbloader.discovery.ShreddedType
+import com.snowplowanalytics.snowplow.rdbloader.common.{S3, LoaderMessage}
+import com.snowplowanalytics.snowplow.rdbloader.common.config.Semver
+import com.snowplowanalytics.snowplow.rdbloader.algebras.db.MigrationBuilder
+import com.snowplowanalytics.snowplow.loader.snowflake.db.ast.SnowflakeDatatype
+import com.snowplowanalytics.snowplow.loader.snowflake.loading.SnowflakeLoader
+import com.snowplowanalytics.snowplow.loader.snowflake.test._
+
+import org.specs2.mutable.Specification
+
+class SnowflakeMigrationSpec extends Specification {
+  import SnowflakeMigrationSpec._
+
+  "build" should {
+    "add new column when column with same name does not exist" in {
+      implicit val dao: SfDao[Pure] = PureDAO.interpreter(PureDAO.init)
+      lazy val migration = new SnowflakeMigrationBuilder[Pure](dbSchema, warehouse)
+
+      val input = List(MigrationBuilder.MigrationItem(shreddedType, schemaListTwo))
+
+      val expected = List(
+        LogEntry.Message(
+          Statement.GetColumns(dbSchema, tableName).toTestString
+        )
+      )
+      val expectedMigration = List(
+        LogEntry.Message(Statement.WarehouseResume(warehouse).toTestString),
+        LogEntry.Message(
+          "Creating new column for schema key iglu:com.acme/some_context/jsonschema/1-0-1"
+        ),
+        LogEntry.Message(
+          Statement.AddColumn(
+            dbSchema,
+            tableName,
+            "UNSTRUCT_EVENT_COM_ACME_SOME_CONTEXT_1",
+            SnowflakeDatatype.JsonObject
+          ).toTestString
+        ),
+        LogEntry.Message(
+          "New column is created for schema key iglu:com.acme/some_context/jsonschema/1-0-1"
+        )
+      )
+
+      val (state, value) = migration.build(input).run
+
+      state.getLog must beEqualTo(expected)
+      value.rethrow must beRight.like {
+        case MigrationBuilder.Migration(pre, in) =>
+          pre.runS.getLog must beEqualTo(expectedMigration)
+          in.runS.getLog must beEmpty
+      }
+    }
+
+    "not add new column when column with same name exists" in {
+      val showColumnRow = Statement.GetColumns.ShowColumnRow(
+        "", "", "UNSTRUCT_EVENT_COM_ACME_SOME_CONTEXT_1", "", "", Some(""), "", Some(""), Some(""), "", Some("")
+      )
+      def getResult(s: TestState)(query: Statement): Any =
+        query match {
+          case Statement.GetColumns(`dbSchema`, `tableName`) => List(showColumnRow)
+          case statement => PureDAO.getResult(s)(statement)
+        }
+      implicit val dao: SfDao[Pure] = PureDAO.interpreter(PureDAO.custom(getResult))
+      lazy val migration = new SnowflakeMigrationBuilder[Pure](dbSchema, warehouse)
+
+      val input = List(MigrationBuilder.MigrationItem(shreddedType, schemaListTwo))
+
+      val expected = List(
+        LogEntry.Message(
+          Statement.GetColumns(dbSchema, tableName).toTestString
+        )
+      )
+      val expectedMigration = List(
+        LogEntry.Message(Statement.WarehouseResume(warehouse).toTestString)
+      )
+
+      val (state, value) = migration.build(input).run
+
+      state.getLog must beEqualTo(expected)
+      value.rethrow must beRight.like {
+        case MigrationBuilder.Migration(pre, in) =>
+          pre.runS.getLog must beEqualTo(expectedMigration)
+          in.runS.getLog must beEmpty
+      }
+    }
+
+    "throw error when given shred type is not supported" in {
+      implicit val dao: SfDao[Pure] = PureDAO.interpreter(PureDAO.init)
+      lazy val migration = new SnowflakeMigrationBuilder[Pure](dbSchema, warehouse)
+
+      val shreddedType = ShreddedType.Tabular(
+        ShreddedType.Info(
+          s3Folder,
+          schemaKey.vendor,
+          schemaKey.name,
+          schemaKey.version.model,
+          Semver(0, 17, 0),
+          LoaderMessage.ShreddedType.SelfDescribingEvent
+        )
+      )
+
+      val input = List(MigrationBuilder.MigrationItem(shreddedType, schemaListTwo))
+
+      val (state, value) = migration.build(input).run
+
+      state.getLog must beEmpty
+      value.rethrow must beLeft.like {
+        case LoaderError.MigrationError(_) => ok
+      }
+    }
+  }
+}
+
+object SnowflakeMigrationSpec {
+  val dbSchema = "public"
+  val tableName = SnowflakeLoader.EventTable
+  val warehouse = "testwarehouse"
+  val schemaKey = SchemaKey("com.acme", "some_context", "jsonschema", Full(1, 0, 0))
+  val schema100 = SelfDescribingSchema(
+    SchemaMap(schemaKey),
+    Schema(
+      properties = Some(
+        ObjectProperty.Properties(
+          Map(
+            "one" -> Schema(),
+            "two" -> Schema()
+          )
+        )
+      )
+    )
+  )
+  val schema101 = SelfDescribingSchema(
+    SchemaMap(schemaKey.copy(version = Full(1, 0, 1))),
+    Schema(
+      properties = Some(
+        ObjectProperty.Properties(
+          Map(
+            "one"   -> Schema(),
+            "two"   -> Schema(),
+            "three" -> Schema()
+          )
+        )
+      )
+    )
+  )
+  val schemaListTwo = createSchemaList(
+    NonEmptyList.of(
+      SnowflakeMigrationSpec.schema100,
+      SnowflakeMigrationSpec.schema101
+    )
+  )
+
+  val s3Folder = S3.Folder.coerce("s3://shredded/archive")
+  val shreddedType = createShreddedType(schemaKey.copy(version = Full(1, 0, 1)))
+
+  def createSchemaList(schemas: NonEmptyList[IgluSchema]): DSchemaList =
+    DSchemaList
+      .unsafeBuildWithReorder(
+        DSchemaList.ModelGroupSet.groupSchemas(schemas).head
+      ).getOrElse(throw new RuntimeException("Cannot create SchemaList"))
+
+  def createShreddedType(schemaKey: SchemaKey,
+                         s3Folder: S3.Folder = SnowflakeMigrationSpec.s3Folder,
+                         shredProperty: LoaderMessage.ShreddedType.ShredProperty = LoaderMessage.ShreddedType.SelfDescribingEvent
+                        ): ShreddedType =
+    ShreddedType.Widerow(
+      ShreddedType.Info(
+        s3Folder,
+        schemaKey.vendor,
+        schemaKey.name,
+        schemaKey.version.model,
+        Semver(0, 17, 0),
+        shredProperty
+      )
+    )
+}
---
+++ modules/snowflake-loader/src/test/scala/com/snowplowanalytics/snowplow/loader/snowflake/loading/SnowflakeLoaderSpec.scala
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2022-2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.loader.snowflake.loading
+
+import com.snowplowanalytics.snowplow.rdbloader.common.{LoaderMessage, Common}
+import com.snowplowanalytics.snowplow.rdbloader.common.config.ShredderConfig.Compression
+import com.snowplowanalytics.snowplow.rdbloader.common.config.Semver
+import com.snowplowanalytics.snowplow.rdbloader.config.components.PasswordConfig
+import com.snowplowanalytics.snowplow.rdbloader.discovery.{ShreddedType, DataDiscovery}
+import com.snowplowanalytics.snowplow.rdbloader.LoaderError
+import com.snowplowanalytics.snowplow.rdbloader.SpecHelpers.AsSql
+import com.snowplowanalytics.snowplow.rdbloader.test.TestState.LogEntry
+import com.snowplowanalytics.snowplow.rdbloader.test._
+
+import com.snowplowanalytics.snowplow.loader.snowflake.config.SnowflakeTarget
+import com.snowplowanalytics.snowplow.loader.snowflake.db.{SfDao, Statement}
+import com.snowplowanalytics.snowplow.loader.snowflake.db.ast.AtomicDef
+import com.snowplowanalytics.snowplow.loader.snowflake.test._
+
+import org.specs2.mutable.Specification
+
+class SnowflakeLoaderSpec extends Specification {
+  import SnowflakeLoaderSpec._
+
+  "run" should {
+    "perform COPY" >> {
+      implicit val dao: SfDao[Pure] = PureDAO.interpreter(PureDAO.init)
+      lazy val loader = new SnowflakeLoader[Pure](target)
+
+      val shreddedTypes = List(
+        ShreddedType.Widerow(
+          ShreddedType.Info(base.dir, "com.acme", "event", 1, shredJobVersion, LoaderMessage.ShreddedType.SelfDescribingEvent)
+        ),
+        ShreddedType.Widerow(
+          ShreddedType.Info(base.dir, "com.acme", "context", 1, shredJobVersion, LoaderMessage.ShreddedType.SelfDescribingEvent)
+        )
+      )
+      val discovery = DataDiscovery(base.dir, shreddedTypes, Compression.Gzip)
+
+      val (state, result) = loader.run(discovery).run
+
+      val expected = List(
+        LogEntry.Message(Statement.WarehouseResume(target.warehouse).toTestString),
+        LogEntry.Message(s"Loading $base"),
+        LogEntry.Message("setStage copying into events table"),
+        LogEntry.Message("COPY events"),
+        LogEntry.Message(
+          Statement.CopyInto(
+            target.schema,
+            SnowflakeLoader.EventTable,
+            target.transformedStage,
+            SnowflakeLoader.getColumns(discovery),
+            loadPath,
+            target.maxError
+          ).toTestString
+        ),
+        LogEntry.Message(s"Folder [$base] has been loaded (not committed yet)")
+      )
+
+      state.getLog must beEqualTo(expected)
+      result must beRight
+    }
+
+    "throw error when discovery contains shredded types with unsupported formats" >> {
+      implicit val dao: SfDao[Pure] = PureDAO.interpreter(PureDAO.init)
+      lazy val loader = new SnowflakeLoader[Pure](target)
+
+      val shreddedTypes = List(
+        ShreddedType.Widerow(
+          ShreddedType.Info(base.dir, "com.acme", "event", 1, shredJobVersion, LoaderMessage.ShreddedType.SelfDescribingEvent)
+        ),
+        ShreddedType.Tabular(
+          ShreddedType.Info(base.dir, "com.acme", "context", 1, shredJobVersion, LoaderMessage.ShreddedType.SelfDescribingEvent)
+        )
+      )
+      val discovery = DataDiscovery(base.dir, shreddedTypes, Compression.Gzip)
+
+      val (state, result) = loader.run(discovery).run
+
+      state.getLog must beEmpty
+      result must beLeft.like {
+        case LoaderError.StorageTargetError(_) => ok
+      }
+    }
+  }
+
+  "getColumn" should {
+    "return list which contains atomic columns and columns for shredded types in discovery" >> {
+      val shreddedTypes = List(
+        ShreddedType.Widerow(
+          ShreddedType.Info(base.dir, "com.acme", "event", 1, shredJobVersion, LoaderMessage.ShreddedType.SelfDescribingEvent)
+        ),
+        ShreddedType.Widerow(
+          ShreddedType.Info(base.dir, "com.acme", "context", 1, shredJobVersion, LoaderMessage.ShreddedType.Contexts)
+        )
+      )
+      val discovery = DataDiscovery(base.dir, shreddedTypes, Compression.Gzip)
+
+      val columns = SnowflakeLoader.getColumns(discovery)
+
+      val expected = AtomicDef.columns.map(_.name) :::
+        List(
+          "unstruct_event_com_acme_event_1",
+          "contexts_com_acme_context_1"
+        )
+
+      columns must beEqualTo(expected)
+    }
+  }
+
+  "Helper methods of CopyInto" should {
+    val columns = List("col1", "col2", "col_3")
+    "return in expected format" >> {
+      val copyInto = Statement.CopyInto(
+        target.schema,
+        SnowflakeLoader.EventTable,
+        target.transformedStage,
+        columns,
+        loadPath,
+        target.maxError
+      )
+
+      val expectedColumnsForCopy = "col1,col2,col_3"
+      val expectedColumnsForSelect = "$1:col1,$1:col2,$1:col_3"
+
+      copyInto.columnsForCopy must beEqualTo(expectedColumnsForCopy)
+      copyInto.columnsForSelect must beEqualTo(expectedColumnsForSelect)
+    }
+  }
+}
+
+object SnowflakeLoaderSpec {
+  val target = SnowflakeTarget(
+    "us-west-2",
+    "sf-user",
+    PasswordConfig.PlainText("sf-pass"),
+    "sf-account",
+    "sf-warehouse",
+    "sf-database",
+    "sf-schema",
+    "sf-transformed-stage",
+    "sf-loader",
+    Some("sf-monitoring-stage"),
+    Some(10),
+    None
+  )
+  val runId = "run=1"
+  val loadPath = s"$runId/${Common.GoodPrefix}"
+  val base = s"s3://bucket/path/$runId/"
+  val shredProperty = LoaderMessage.ShreddedType.SelfDescribingEvent
+  val shredJobVersion = Semver(1, 5, 0)
+}
