968ee25c61bda71ee692d63a0dcfd5d55e770310
Scala Steward <me@scala-steward.org>
2022-03-30 01:04:37 +0200

    Bump aws-java-sdk-dynamodb, ... from 1.12.161 to 1.12.188

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
     // Java (Loader)
     val slf4j            = "1.7.32"
     val redshift         = "1.2.55.1083"
-    val aws              = "1.12.161"
+    val aws              = "1.12.188"
     val aws2             = "2.17.59"
     val jSch             = "0.1.55"
     val sentry           = "1.7.30"
