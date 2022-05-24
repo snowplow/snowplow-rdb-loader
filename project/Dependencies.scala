d4ee8116b595698a8184baeb80933413d2c9a10b
spenes <enes@snowplowanalytics.com>
2022-05-25 00:35:54 +0300

    Common: bump http4s to 0.21.33 (close #902)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     val monocle          = "2.0.3"
     val catsRetry        = "2.1.0"
     val log4cats         = "1.3.0"
-    val http4s           = "0.21.25"
+    val http4s           = "0.21.33"
     val scalaTracker     = "1.0.0"
 
     val spark            = "3.1.2"
