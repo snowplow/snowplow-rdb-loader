3f72f322ece6134d157c65b0c6515e52414cd4ae
Ian Streeter <ian@snowplowanalytics.com>
2022-06-13 13:09:48 +0100

    Add logging around using SSH tunnel (close #923)

--- modules/loader/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/utils/SSH.scala
+++ modules/loader/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/utils/SSH.scala
@@ -13,24 +13,40 @@
 package com.snowplowanalytics.snowplow.rdbloader.utils
 
 import cats.Monad
-import cats.effect.{Resource, Sync}
-import cats.implicits._
-
-import com.jcraft.jsch.{JSch, Session}
-
+import cats.effect.{ConcurrentEffect, Resource, Sync}
+import cats.syntax.all._
+import cats.effect.syntax.all._
+import com.jcraft.jsch.{JSch, Session, Logger => JLogger}
 import com.snowplowanalytics.snowplow.rdbloader.dsl.AWS
-
 import com.snowplowanalytics.snowplow.rdbloader.config.StorageTarget.TunnelConfig
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object SSH {
+
+  implicit def unsafeLogger[F[_]: Sync] = Slf4jLogger.getLogger[F]
 
   /** Actual SSH identity data. Both passphrase and key are optional */
   case class Identity(passphrase: Option[Array[Byte]], key: Option[Array[Byte]])
 
   /** Open SSH tunnel, which will be guaranteed to be closed when application exits */
-  def resource[F[_]: Sync: AWS](tunnelConfig: Option[TunnelConfig]): Resource[F, Unit] =
+  def resource[F[_]:ConcurrentEffect: Sync: AWS](tunnelConfig: Option[TunnelConfig]): Resource[F, Unit] =
     tunnelConfig match {
       case Some(tunnel) =>
+        Resource.eval{
+
+          Sync[F].delay(JSch.setLogger(new JLogger{
+          override def isEnabled(level: Int): Boolean = true
+
+          override def log(level: Int, message: String): Unit = level match {
+            case JLogger.INFO => Logger[F].info("JCsh: " + message).toIO.unsafeRunSync()
+            case JLogger.ERROR => Logger[F].error("JCsh: " + message).toIO.unsafeRunSync()
+            case JLogger.DEBUG => Logger[F].debug("JCsh: " + message).toIO.unsafeRunSync()
+            case JLogger.WARN => Logger[F].warn("JCsh: " + message).toIO.unsafeRunSync()
+            case JLogger.FATAL => Logger[F].error("JCsh: " + message).toIO.unsafeRunSync()
+            case _ => Logger[F].warn("NO LOG LEVEL JCsh: " + message).toIO.unsafeRunSync()
+          }
+        }))} >>
         Resource.make(getIdentity[F](tunnel).flatMap(i => createSession(tunnel, i)))(s => Sync[F].delay(s.disconnect())).void
       case None =>
         Resource.pure[F, Unit](())
@@ -51,7 +67,9 @@ object SSH {
    */
   def createSession[F[_]: Sync](tunnelConfig: TunnelConfig, identity: Identity): F[Session] =
     Sync[F].delay {
-      val jsch = new JSch()
+      val jsch = new JSch() {
+        val _: Identity = identity
+      }
       jsch.addIdentity("rdb-loader-tunnel-key", identity.key.orNull, null, identity.passphrase.orNull)
       val sshSession = jsch.getSession(tunnelConfig.bastion.user, tunnelConfig.bastion.host, tunnelConfig.bastion.port)
       sshSession.setConfig("StrictHostKeyChecking", "no")
