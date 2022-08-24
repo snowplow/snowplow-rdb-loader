9babca62f8e3d92091c96c21ae864287da3c9123
Scala Steward <scala_steward@virtuslab.com>
2022-08-24 19:13:03 +0000

    Bump snowflake-jdbc from 3.13.9 to 3.13.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
 
     val slf4j            = "1.7.32"
     val redshiftJdbc     = "1.2.55.1083"
-    val snowflakeJdbc    = "3.13.9"
+    val snowflakeJdbc    = "3.13.22"
     val enumeratum       = "1.7.0"
     val aws              = "1.12.161"
     val aws2             = "2.17.59"
