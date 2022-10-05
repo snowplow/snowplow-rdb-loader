4a3c581837e7811c46712c21d15158a0d27fd59c
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2022-10-05 17:40:52 +0300

    Loader: add secret manager

---
+++ modules/gcp/src/main/scala/com/snowplowanalytics/snowplow/rdbloader/gcp/SecretManager.scala
@@ -0,0 +1,20 @@
+package com.snowplowanalytics.snowplow.rdbloader.gcp
+
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient
+
+import cats.effect.{Resource, Sync}
+
+import com.snowplowanalytics.snowplow.rdbloader.common.cloud.SecretStore
+
+
+object SecretManager {
+
+  def secretManager[F[_]: Sync]: SecretStore[F] = new SecretStore[F] {
+    override def getValue(key: String): F[String] =
+      Resource
+        .fromAutoCloseable(Sync[F].delay(SecretManagerServiceClient.create()))
+        .use { client =>
+          Sync[F].delay(client.accessSecretVersion(key).getPayload.getData.toStringUtf8)
+        }
+  }
+}
