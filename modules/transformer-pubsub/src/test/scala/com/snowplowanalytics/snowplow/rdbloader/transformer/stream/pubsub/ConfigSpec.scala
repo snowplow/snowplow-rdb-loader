73483ed2feb1f1eb84d60210df8d831c95a42505
pavel.voropaev <voropaev.p@gmail.com>
2023-01-16 16:21:35 +0000

    Parquet transformer // set event limit per partition (close #1178)

--- modules/transformer-pubsub/src/test/scala/com/snowplowanalytics/snowplow/rdbloader/transformer/stream/pubsub/ConfigSpec.scala
+++ modules/transformer-pubsub/src/test/scala/com/snowplowanalytics/snowplow/rdbloader/transformer/stream/pubsub/ConfigSpec.scala
@@ -132,7 +132,7 @@ object ConfigSpec {
       None,
       None
     )
-  val exampleDefaultFeatureFlags = TransformerConfig.FeatureFlags(false, None)
+  val exampleDefaultFeatureFlags = TransformerConfig.FeatureFlags(false, None, true)
   val exampleValidations = Validations(Some(Instant.parse("2021-11-18T11:00:00.00Z")))
   val emptyValidations = Validations(None)
   val TestProcessor = Processor(BuildInfo.name, BuildInfo.version)
