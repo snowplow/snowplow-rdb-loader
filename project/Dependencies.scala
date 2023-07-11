12cc0d3e6c45bd67e31d3ab012eed92db8c7b62c
spenes <enes@snowplowanalytics.com>
2023-07-11 17:50:09 +0300

    Bump nimbus-jose-jwt to 9.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,6 +52,7 @@ object Dependencies {
     val hadoopGcpClient  = "hadoop3-2.2.5"
     val parquetHadoop    = "1.12.3"
     val jsonSmart        = "2.4.9" // Fix CVE
+    val nimbusJose       = "9.31" // Fix CVE
     val snappyJava       = "1.1.10.1" // Fix CVE
     val commonsText      = "1.10.0" // Fix CVE
     val ivy              = "2.5.1" // Fix CVE
@@ -141,6 +142,7 @@ object Dependencies {
     .exclude("com.amazonaws", "aws-java-sdk-bundle") // aws-java-sdk-core is already present in assembled jar
   val hadoopGcp         = "com.google.cloud.bigdataoss"      % "gcs-connector"             % V.hadoopGcpClient % Runtime
   val jsonSmart         = "net.minidev"                      % "json-smart"                % V.jsonSmart
+  val nimbusJose        = ("com.nimbusds"                    % "nimbus-jose-jwt"           % V.nimbusJose)
   val snappyJava        = "org.xerial.snappy"                % "snappy-java"               % V.snappyJava
   val kinesisClient     = ("software.amazon.kinesis"         %  "amazon-kinesis-client"    % V.kinesisClient)
                           .exclude("software.amazon.glue", "schema-registry-common")
@@ -306,6 +308,7 @@ object Dependencies {
     hadoop,
     parquetHadoop,
     jsonSmart,
+    nimbusJose,
     snappyJava,
     specs2,
     specs2ScalaCheck,
