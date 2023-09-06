4cba713dfb2a460ea8f08f1f18cadd6c931ae294
spenes <enes@snowplowanalytics.com>
2023-09-06 16:22:51 +0300

    Bump snappy-java to 1.1.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
     val parquetHadoop    = "1.12.3"
     val jsonSmart        = "2.4.9" // Fix CVE
     val nimbusJose       = "9.31" // Fix CVE
-    val snappyJava       = "1.1.10.1" // Fix CVE
+    val snappyJava       = "1.1.10.3" // Fix CVE
     val commonsText      = "1.10.0" // Fix CVE
     val ivy              = "2.5.1" // Fix CVE
 
@@ -280,6 +280,7 @@ object Dependencies {
     specs2ScalaCheck,
     scalaCheck,
     jettison,
+    snappyJava,
     catsEffectLaws,
     catsTesting,
     catsEffectTestkit
