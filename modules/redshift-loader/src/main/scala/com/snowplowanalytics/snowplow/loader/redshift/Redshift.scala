6984e14df15e5001a2e3cf0ea2d7f2e8dc731438
colmsnowplow <colm@snowplowanalytics.com>
2024-01-17 11:54:14 +0000

    Patch missed variable in renaming to Recovery

--- modules/redshift-loader/src/main/scala/com/snowplowanalytics/snowplow/loader/redshift/Redshift.scala
+++ modules/redshift-loader/src/main/scala/com/snowplowanalytics/snowplow/loader/redshift/Redshift.scala
@@ -114,7 +114,7 @@ object Redshift {
                     loadAuthMethod,
                     discoveredShredModels.shredModel,
                     tableName,
-                    isRecovery && !isMigrationDisabled
+                    isRecovery && !isRecoveryDisabled
                   )
               }
               .toList
